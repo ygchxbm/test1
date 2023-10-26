11e6c9a734c6912013d1ac843e6c6ca56f164183
hugh <2401874452@qq.com>
2023-10-26 10:39:12 +0800

    新增add页面

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Add from './components/Add.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <Add />
 </template>
 
 <style scoped>
